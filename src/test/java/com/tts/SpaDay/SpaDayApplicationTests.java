b5a28175ea67e5f40429dedbf4a810767d0ea505
rachelcarr37 <rachel.carr37@gmail.com>
2020-12-07 12:15:39 -0500

    first commit

---
+++ src/test/java/com/tts/SpaDay/SpaDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.SpaDay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
